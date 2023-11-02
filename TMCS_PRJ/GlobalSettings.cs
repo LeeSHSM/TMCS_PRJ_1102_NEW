5399a989c4d21bcb248641cbe22d35f56bfdecef
LeeSH <igo2006@naver.com>
2023-11-02 19:37:06 +0900

    초기 커밋.

---
+++ TMCS_PRJ/GlobalSettings.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMCS_PRJ
+{
+    public class GlobalSettings
+    {
+
+        public static Logger Logger { get; } = new Logger();
+        public enum ChannelType
+        {
+            INPUT,
+            OUTPUT,
+        }
+    }
+
+    public class Logger
+    {
+        private readonly string logFilePath;
+        public Logger()
+        {
+            string logDirectory = Directory.GetCurrentDirectory() + "\\log";
+            if (!Directory.Exists(logDirectory))
+            {
+                Directory.CreateDirectory(logDirectory);
+            }
+
+            logFilePath = Path.Combine(logDirectory, "log" + $"({DateTime.Now:yyyy-MM-dd})" + ".txt");
+        }
+
+        public void LogError(string message)
+        {
+            Log("ERROR", message);
+        }
+
+        public void LogInfo(string message)
+        {
+            Log("INFO", message);
+        }
+
+        object ojb = new object();
+        private void Log(string logLevel, string message)
+        {
+            lock (ojb)
+            {
+                string logMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss} [{logLevel}] {message}";
+                File.AppendAllText(logFilePath, logMessage + Environment.NewLine);
+            }
+        }
+    }
+}
---
+++ TMCS_PRJ/Matrix.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMCS_PRJ
+{
+    public class MatrixChannel : Label
+    {
+        public event EventHandler ChangeNameEvent;
+
+        #region Properties
+        private int _portNo;
+        private string _channelName;
+        private string _channelType;
+        private int _inToOutRouteNo;
+        public int Port
+        {
+            get => _portNo;
+            set
+            {               
+                _portNo = value;
+            }
+        }
+        public string ChannelName
+        {
+            get => _channelName;
+            set
+            {
+                _channelName = value;
+                ChangeNameEvent?.Invoke(this, EventArgs.Empty);
+            }
+        }
+        public string ChannelType
+        {
+            get => _channelType;
+            set
+            {
+                _channelType = value;
+            }
+        }
+        /// <summary>
+        /// 인풋채널에만 있음.. 인풋채널은 0 고정, out채널은 0은 신호없음, 나머진 1~ 최대 아웃카운트중
+        /// </summary>
+        public int InToOutRouteNo
+        {
+            get => _inToOutRouteNo;
+            set
+            {
+                _inToOutRouteNo = value;
+            }
+        }
+        #endregion
+    }
+
+    public class Matrix
+    {
+        #region Properties
+        private int _inputChannelPortCount;
+        private int _outputChannelPortCount;
+        private string _videoType;
+        private List<MatrixChannel> _inputChannels;
+        private List<MatrixChannel> _outputChannels;
+
+        public int InputChannelCount { get => _inputChannelPortCount; }
+        public int OutputChannelCount { get => _outputChannelPortCount; }
+        public List<MatrixChannel> InputChannel { get => _inputChannels; set => _inputChannels = value; }
+        public List<MatrixChannel> OutputChannel { get => _outputChannels; set => _outputChannels = value; }
+        public string VideoType { get => _videoType; }
+        #endregion
+
+        #region 생성자
+        public Matrix(int inputChannelCount, int outputChannelCount, string videoType)
+        {
+            _inputChannelPortCount = inputChannelCount;
+            _outputChannelPortCount = outputChannelCount;
+            _inputChannels = new List<MatrixChannel>();
+            _outputChannels = new List<MatrixChannel>();
+            _videoType = videoType;
+        }
+        #endregion
+
+        #region Public Methods
+        public int getChannelPortCount(string channelType)
+        {
+            if (channelType == GlobalSetting.ChannelType.INPUT.ToString())
+            {
+                return _inputChannelPortCount;
+            }
+            else if (channelType == GlobalSetting.ChannelType.OUTPUT.ToString())
+            {
+                return _outputChannelPortCount;
+            }
+
+            throw new ArgumentException("Invalid channel type", nameof(channelType));
+        }
+        #endregion
+
+        #region Private Methods
+        private void InitializeChannels()
+        {
+            for (int i = 0; i < _inputChannelPortCount; i++)
+            {
+                _inputChannels.Add(new MatrixChannel { Port = i + 1, ChannelType = "INPUT" });
+            }
+
+            for (int i = 0; i < _outputChannelPortCount; i++)
+            {
+                _outputChannels.Add(new MatrixChannel { Port = i + 1, ChannelType = "OUTPUT" });
+            }
+        }
+        #endregion
+    }
+}
